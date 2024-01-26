7c49e42622a0adcecaba521ac4e9ea6504f03157
naoki316 <takehara316@me.com>
2024-01-26 09:36:12 +0000

    7.2終了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,25 +1,25 @@
 module SessionsHelper
-  
+
   # 引数に渡されたユーザーオブジェクトでログインします。
   def log_in(user)
     session[:user_id] = user.id
   end
-  
-  # 永続的セッションを記憶します。（Userモデルを参照）
+
+  # 永続的セッションを記憶します（Userモデルを参照）
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-  
-  # セッションと@current_userを破棄します。
+
+  # セッションと@current_userを破棄します
   def log_out
     session.delete(:user_id)
     @current_user = nil
   end
 
-  
-  # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
+  # 一時的セッションにいるユーザーを返します。
+  # それ以外の場合はcookiesに対応するユーザーを返します。
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -31,7 +31,7 @@ module SessionsHelper
       end
     end
   end
-  
+
   # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
   def logged_in?
     !current_user.nil?
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,13 @@ class User < ApplicationRecord
   # 「remember_token」という仮想の属性を作成します。
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
-  
-  validates :name, presence: true, length: { maximum: 50 }
+
+  validates :name,  presence: true, length: { maximum: 50 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
-             format: { with: VALID_EMAIL_REGEX },
-             uniqueness: true
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true    
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
@@ -26,16 +27,15 @@ class User < ApplicationRecord
   def User.new_token
     SecureRandom.urlsafe_base64
   end
-  
+
   # 永続セッションのためハッシュ化したトークンをデータベースに記憶します。
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
-  
+
   # トークンがダイジェストと一致すればtrueを返します。
-  def authentivated?(remember_token)
+  def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
-
 end
