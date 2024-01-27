2590392aa59a956e148ad275683f532ffd091fd0
naoki316 <takehara316@me.com>
2024-01-27 09:38:19 +0000

    Finish editing-users

--- Gemfile
+++ Gemfile
@@ -1,10 +1,10 @@
 source "https://rubygems.org"
 
+
 ruby "3.2.1"
 gem "rails", "~> 7.1.0"
 gem 'bcrypt'
 gem 'faker'
-gem 'bootstrap-sass'
 gem "sprockets-rails"
 gem "sqlite3", "~> 1.4"
 gem "puma", ">= 5.0"
@@ -13,18 +13,38 @@ gem "turbo-rails"
 gem "stimulus-rails"
 gem "jbuilder"
 gem "tzinfo-data", platforms: %i[ mswin mswin64 mingw x64_mingw jruby ]
-
 gem "bootsnap", require: false
+gem "sassc-rails"
+gem 'turbo-rails'
+gem 'bootstrap-sass', '3.3.6'
+gem 'jquery-rails'
+gem 'will_paginate', '~> 3.3' # この行を追加してください。
+gem 'bootstrap-will_paginate', '1.0.0' # この行を追加してください。
+
+
 
 group :development, :test do
   gem "debug", platforms: %i[ mri mswin mswin64 mingw x64_mingw ]
 end
 
+
 group :development do
   gem "web-console"
 end
 
+
 group :test do
-  gem "capybara"
-  gem "selenium-webdriver"
+ gem 'capybara', '>= 3.26'
+ gem 'selenium-webdriver'
+ gem 'webdrivers'
+ gem 'rails-flog', require: 'flog'
+ gem 'rspec-rails'
+ gem "factory_bot_rails"
+ gem 'faker'
+ gem 'database_cleaner'
+ gem 'rails-controller-testing'
 end
+
+# Windows環境ではtzinfo-dataというgemを含める必要があります
+# Mac環境でもこのままでOKです
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :logged_in_user, only: [:index, :show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def show
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -21,34 +22,41 @@ class UsersController < ApplicationController
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
-      render 'new', status: :unprocessable_entity
+     render 'new', status: :unprocessable_entity
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @user.update(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
-      render 'edit', status: :unprocessable_entity
+      render 'edit', status: :unprocessable_entity    
     end
   end
 
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end
+
   private
+
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-    
+
     # beforeフィルター
-    
+
     # paramsハッシュからユーザーを取得します。
     def set_user
       @user = User.find(params[:id])
     end
-    
+
     # ログイン済みのユーザーか確認します。
     def logged_in_user
       unless logged_in?
@@ -57,9 +65,14 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
-    
+
     # アクセスしたユーザーが現在ログインしているユーザーか確認します。
     def correct_user
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # システム管理権限所有かどうか判定します。
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
