1da710527e3a8790e6761c936249a07d7da1ed45
naoki316 <takehara316@me.com>
2024-02-08 09:30:36 +0000

    勤怠B完成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
       page_name + "|" + base_title
     end
   end
+  
+  def round_to_nearest_15_minutes(time)
+    return nil if time.nil?
+
+    minutes = time.min
+    rounded_minutes = (minutes / 15.0).round * 15
+    time.change(min: rounded_minutes)
+  end
+  
 end
